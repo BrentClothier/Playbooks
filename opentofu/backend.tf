1267b18b1f9d9f94a5185e88809705f5c06130f5
Brent Clothier <BrentMClothier@gmail.com>
2025-11-26 10:35:57 -0800

    changes

---
+++ opentofu/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    endpoint = "https://minio.clothiernet.duckdns.org"
+
+    bucket = "terraform-state"
+    key    = "proxmox/homelab.tfstate"
+    region = "us-east-1"
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    force_path_style            = true
+  }
+}
