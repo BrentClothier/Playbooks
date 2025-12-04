a6dbf8823ca3b99806545904e0d8b91bc28c7cea
Brent Clothier <BrentMClothier@gmail.com>
2025-12-04 10:19:26 -0800

    blach

---
+++ opentofu/lxc.tf
@@ -0,0 +1,21 @@
+resource "proxmox_lxc" "test_container" {
+  target_node = "proxmox3"
+  hostname    = "test-lxc"
+  ostemplate  = "local:vztmpl/ubuntu-25.04-standard_25.04-1.1_amd64.tar.zst"
+
+  cores  = 1
+  memory = 512  # MB
+
+  rootfs {
+    storage = "local"
+    size    = "4G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+
+  onboot = false
+}
